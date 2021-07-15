3156db29be655b33453f6deb9a28a0c31f18b11d
coralcry <constshatalov@gmail.com>
2021-07-15 23:35:40 +0400

    Initial commit (working app).

---
+++ README.md
@@ -0,0 +1,16 @@
+# WaveBot
+## Discord bot that encourages people to say hi 👋 to each other!
+
+WaveBot temporarily adds a marker to user's nickname when they join a voice channel.
+
+### Usage
+```bash
+$ npm install
+$ WAVEBOT_TOKEN=your_discord_bot_token node index.js
+```
+
+### TODO
+* Add timestamps to console logs
+* Add command interface to set prefix string, clear all nicknames
+* Use PM2 to handle Node on the server
+* Graceful shutdown (e.g. reset nicknames on shutdown)
---
+++ index.js
@@ -0,0 +1,54 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const namePrefix = '👋'
+const namePrefixTimeout = 7000
+
+let timeoutFunction = null
+
+client.on('ready', () => { console.log('Ready.') })
+
+function userInfo(member) {
+    return `${member.user.tag} <@!${member.user.id}>`
+}
+
+function setNicknameForMember(nickname, member) {
+    member.setNickname(nickname).then(message => {
+        console.log(`Set nickname '${nickname}' for member ${userInfo(member)}. ${message}`)
+    }).catch(message => {
+        console.log(`Failed to set nickname '${nickname}' for member ${userInfo(member)}. ${message}`)
+    })
+}
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+    const userLeftChannel = newState.channel === null
+    const userJoinedChannel = (oldState.channel != newState.channel) && !userLeftChannel
+    const { displayName } = newState.member
+
+    if (userJoinedChannel) {
+        console.log(`${newState.member.user.tag} joined '${newState.channel.name}'`)
+
+        clearTimeout(timeoutFunction)
+
+        if (!displayName.startsWith(namePrefix)) {
+            setNicknameForMember(`${namePrefix}${displayName}`, newState.member)
+        }
+
+        timeoutFunction = setTimeout(() => {
+            setNicknameForMember(`${displayName.replace(namePrefix, '')}`, newState.member)
+        }, namePrefixTimeout)
+    } else if (userLeftChannel) {
+        console.log(`${newState.member.user.tag} left '${oldState.channel.name}'`)
+        clearTimeout(timeoutFunction)
+
+        if (displayName.startsWith(namePrefix)) {
+            setNicknameForMember(`${displayName.replace(namePrefix, '')}`, newState.member)
+        }
+    }
+})
+
+client.login(process.env.WAVEBOT_TOKEN)
+
+/* Remote server instructions (SSH) */
+// * To upload all local files to server: $ scp -r * root@178.62.82.252:/root/bot/
+// * SSH into server: $ ssh root@[ip-adress]
