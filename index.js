91bff9e82e775a5bb2162ef8d5a99adb15d187f6
coralcry <constshatalov@gmail.com>
2021-07-21 03:51:00 +0400

    Add graceful shutdown.

--- index.js
+++ index.js
@@ -15,20 +15,13 @@ function memberInfo(member) {
     return `${member.guild.name}: ${member.displayName} (${member.user.tag})`
 }
 
-function setNicknameForMember(nickname, member) {
+function setNicknameForMember(nickname, member, callback) {
     member.setNickname(nickname).then(message => {
-        console.log(`${moment().format()} -> Set nickname '${nickname}' for member ${member.displayName}. ${message}`)
+        console.log(`${moment().format()} Set nickname '${nickname}' for member ${member.displayName}. ${message}`)
+        if (typeof callback === 'function' && callback()) callback()
     }).catch(message => {
-        console.log(`${moment().format()} -x Failed to set nickname '${nickname}' for member ${member.displayName}. ${message}`)
-    })
-}
-
-function shutdown() {
-    console.log('-! Gracefully shutting down. Removing nickname prefixes...')
-
-    timers.forEach((value, key) => {
-        setNicknameForMember(`${key.displayName.replace(namePrefix, '')}`, key)
-        timers.delete(key)
+        console.log(`${moment().format()} Failed to set nickname '${nickname}' for member ${member.displayName}. ${message}`)
+        if (typeof callback === 'function' && callback()) callback()
     })
 }
 
@@ -39,9 +32,9 @@ client.on('voiceStateUpdate', (oldState, newState) => {
 
     if (userJoinedChannel) {
         if (!oldState.channel) {
-            console.log(`${moment().format()} ${memberInfo(newState.member)} joined ${newState.channel.name}`)
+            console.log(`${moment().format()} -> ${memberInfo(newState.member)} joined ${newState.channel.name}`)
         } else {
-            console.log(`${moment().format()} ${memberInfo(newState.member)} moved to ${newState.channel.name} from ${oldState.channel.name}`)
+            console.log(`${moment().format()} -> ${memberInfo(newState.member)} moved to ${newState.channel.name} from ${oldState.channel.name}`)
         }
 
         if (timers.get(newState.member)) {
@@ -55,11 +48,12 @@ client.on('voiceStateUpdate', (oldState, newState) => {
 
         let timer = setTimeout(() => {
             setNicknameForMember(`${displayName.replace(namePrefix, '')}`, newState.member)
+            timers.delete(newState.member)
         }, namePrefixTimeoutLength)
 
         timers.set(newState.member, timer)
     } else if (userLeftChannel) {
-        console.log(`${moment().format()} ${memberInfo(newState.member)} left ${oldState.channel.name}`)
+        console.log(`${moment().format()} <- ${memberInfo(newState.member)} left ${oldState.channel.name}`)
 
         if (timers.get(newState.member)) {
             clearTimeout(timers.get(newState.member))
@@ -73,3 +67,35 @@ client.on('voiceStateUpdate', (oldState, newState) => {
 })
 
 client.login(process.env.WAVEBOT_TOKEN)
+
+/* Graceful shutdown */
+function shutdown(callback) {
+    const totalNumberOfTimers = timers.size
+    let numberOfTimersDeleted = 0
+
+    if (totalNumberOfTimers) {
+        console.log(`${totalNumberOfTimers} hanging timers found, cleaning up...`)
+        timers.forEach((value, key) => {
+            setNicknameForMember(`${key.displayName.replace(namePrefix, '')}`, key, () => {
+                timers.delete(key)
+                numberOfTimersDeleted++
+
+                if (numberOfTimersDeleted === totalNumberOfTimers) {
+                    callback(0)
+                    return
+                }
+            })
+        })
+    } else {
+        console.log(`No hanging timers found.`)
+        callback(0)
+    }
+}
+
+process.on('SIGINT', () => {
+    console.log('Gracefully shutting down...')
+    shutdown((code) => {
+        console.log(`Shutdown complete.`)
+        process.exit(code)
+    })
+})
