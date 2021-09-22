2dac64d753a4ca7123b099a884f7d0d41802c1f7
coralcry <constshatalov@gmail.com>
2021-09-22 05:31:44 +0400

    Fix token leak

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,11 @@ module.exports = {
         watch: true,
         kill_timeout: 3000,
         env: {
-            "WAVEBOT_TOKEN": "ODY0OTk1NzQ4MzQ0MzY1MTE2.YO9kEQ.b94AZL4FS_MDRIAyGiWFzcYt2cA",
+            "WAVEBOT_TOKEN": "ODY0OTk1NzQ4MzQ0MzY1MTE2.YO9kEQ.Lu5lbr0WfGi13gX_sH1c0dGct_c",
             "NODE_ENV": "development"
         },
         env_production: {
-            "WAVEBOT_TOKEN": "ODY0OTQ5MjI4NTMzOTcyOTky.YO84vg.65cNbETaeS5PrK5w5sE1Lnp4lQU",
+            "WAVEBOT_TOKEN": "ODY0OTQ5MjI4NTMzOTcyOTky.YO84vg.ZGPUWGCC6daUL-sVf7GklJ9I6bM",
             "NODE_ENV": "production"
         }
     }]
